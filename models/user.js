811503ebd31a43ed224edb217f91a79fd445ebdf
WarrenSadler3190 <warren.sadler@leankit.com>
2013-11-18 14:13:42 -0600

    added header and background image

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var User = mongoose.Schema({
   name: String,
-  band: [{type:mongoose.Schema.Types.ObjectID, ref: "Band"}],
+  // band: [{type:mongoose.Schema.Types.ObjectID, ref: "Band"}],
   music: [],
   image: String,
   createdAt: {type: Date, default: Date.now}
