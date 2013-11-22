9aab300a0cce9bd1fd19eb29e82ad6245a74428b
WarrenSadler3190 <warren.sadler@leankit.com>
2013-11-22 14:08:46 -0600

    looked at a dump

--- sockets/common.js
+++ sockets/common.js
@@ -1,22 +1,22 @@
 exports.connection = function(socket){
   socket.emit('connected', {status: 'connected'});
   socket.on('disconnect', socketDisconnect);
+  socket.on('message', function (data) {
+    if(data.message) {
+        messages.push(data);
+        var html = '';
+        for(var i=0; i<messages.length; i++) {
+            html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+            html += messages[i].message + '<br />';
+        }
+        content.innerHTML = html;
+        $("#content").scrollTop($("#content")[0].scrollHeight);
+    } else {
+        console.log("There is a problem:", data);
+    }
+});
 };
 
 function socketDisconnect(){
 }
 
-// socket.on('message', function (data) {
-//     if(data.message) {
-//         messages.push(data);
-//         var html = '';
-//         for(var i=0; i<messages.length; i++) {
-//             html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-//             html += messages[i].message + '<br />';
-//         }
-//         content.innerHTML = html;
-//         $("#content").scrollTop($("#content")[0].scrollHeight);
-//     } else {
-//         console.log("There is a problem:", data);
-//     }
-// });
