64b530e345eb1e3a5dcd7fbc818d4aab68d00dd1
JL Phillips <phillipsj@pop.belmont.edu>
2013-11-22 14:05:46 -0600

    Updates

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -8,6 +8,7 @@ function initialize(){
   $(document).foundation();
   onLoads();
   clickHandlers();
+  // loadChat();
   initializeSocketIO();
 }
 
@@ -15,7 +16,7 @@ function initialize(){
 
 function onLoads(){
   $('body').hide().fadeIn(4000);
-  $("#chatbox").hide();
+  $('#chatbox').hide();
   $('#chatbox').draggable({revert: false, containment: "parent", scroll: false});
 }
 
@@ -52,23 +53,62 @@ function clickChatSendButton(){
     $message.text(firstname + ": " + comment);
     $("#chatwindow").append($message);
   }
-
   $("#chatinput").val("");
   $("#chatinput").focus();
 }
 
-function chatSendButtonGuts(){
-  if($("#chatinput").val() == ""){
-    alert("Enter text you must, or chat you will not!");
-  }else{
-    var comment = $("#chatinput").val();
-    var firstname = "Heisenburg";
-    var $message = $("<div>");
-    $message.addClass("message");
-    $message.text(firstname + ": " + comment);
-    $("#chatwindow").append($message);
-  }
-}
+// function clickChatSendButton(){
+//   var socket = io.connect('http://localhost:3700');
+//   var comment = $("#chatfield").val();
+//   var name = Jack;
+//   var messages = [];
+
+//   if($("#chatinput").val() == ""){
+//     alert("Enter text you must, or chat you will not!");
+//   }else{
+//     socket.emit('send', {message: comment, username: name.value});
+//   }
+
+//   $("#chatinput").val("");
+//   $("#chatinput").focus();
+// }
+
+// -----------------------------------------------[Load Chat]----------------->
+
+// function loadChat(){
+//   var messages = [];
+//   var socket = io.connect('http://localhost:3700');
+//   var field = document.getElementById("chatinput");
+//   var sendButton = document.getElementById("chatsendbutton");
+//   var content = document.getElementById("chatinput");
+//   var name = Jack;
+
+//   socket.on('message', function (data) {
+//     if(data.message) {
+//         messages.push(data);
+//         var html = '';
+//         for(var i=0; i<messages.length; i++) {
+//             html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+//             html += messages[i].message + '<br />';
+//         }
+//         content.innerHTML = html;
+//     } else {
+//         console.log("There is a problem:", data);
+//     }
+//   });
+
+//   sendButton.onclick = function() {
+//     if(name.value == "") {
+//         alert("Please type your name!");
+//     } else {
+//         var text = field.value;
+//         socket.emit('send', { message: text, username: name.value });
+
+//     }
+//   };
+// }
+
+// -----------------------------------------------[Initialize Socket.io]----------------->
 
 function initializeSocketIO(){
   var port = window.location.port ? window.location.port : '80';
--- sockets/common.js
+++ sockets/common.js
@@ -1,22 +1,29 @@
 exports.connection = function(socket){
   socket.emit('connected', {status: 'connected'});
   socket.on('disconnect', socketDisconnect);
+
+  // socket.emit('message', { message: 'Band Up!' });
+  // socket.on('send', function (data) {
+  //     io.sockets.emit('message', data);
+  // });
 };
 
 function socketDisconnect(){
 }
 
-// socket.on('message', function (data) {
+// exports.chat = function(socket){
+//   var messages = [];
+//   socket.on('message', function (data) {
 //     if(data.message) {
-//         messages.push(data);
-//         var html = '';
-//         for(var i=0; i<messages.length; i++) {
-//             html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-//             html += messages[i].message + '<br />';
+//       messages.push(data);
+//       var html = '';
+//       for(var i=0; i<messages.length; i++) {
+//           html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+//           html += messages[i].message + '<br />';
 //         }
-//         content.innerHTML = html;
-//         $("#content").scrollTop($("#content")[0].scrollHeight);
+//       content.innerHTML = html;
 //     } else {
-//         console.log("There is a problem:", data);
+//       console.log("There is a problem:", data);
 //     }
-// });
+//   });
+// }
